45ff3cd3ac57a5fec01fcc23a6b0b26ec0a11209
andy.kuszyk <andy.kuszyk@galleria-rts.com>
2016-03-24 14:54:21 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ScriptJoiner
+{
+    class Program
+    {
+        private const String _scriptFileName = "ScriptJoiner.txt";
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo currentDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            String scriptFilePath = Path.Combine(currentDirectory.FullName, _scriptFileName);
+            if (File.Exists(scriptFilePath))
+            {
+                File.Delete(scriptFilePath);
+            }
+            List<String> scriptLines = new List<String>();
+
+            foreach (FileInfo script in currentDirectory.GetFiles("*.txt", SearchOption.TopDirectoryOnly).OrderBy(f => f.Name))
+            {
+                using (StreamReader scriptStreamReader = new StreamReader(script.OpenRead()))
+                {
+                    while (!scriptStreamReader.EndOfStream)
+                    {
+                        scriptLines.Add(scriptStreamReader.ReadLine());
+                    }
+                }
+            }
+
+            using (StreamWriter scripts = File.CreateText(scriptFilePath))
+            {
+                foreach (String scriptLine in scriptLines)
+	            {
+                    scripts.WriteLine(scriptLine);
+	            }
+            }
+        }
+    }
+}
